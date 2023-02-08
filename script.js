074648ab035942cdee17e47cc6d930a7afc99ded
LucasClaudino <lucas.claudinod@gmail.com>
2023-02-08 18:50:14 -0300

    finalParcial

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ pizzaJson.map((item, index)=>{
 
     pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price[2].toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
@@ -34,7 +34,7 @@ pizzaJson.map((item, index)=>{
         });
         c('.pizzaInfo--qt').innerHTML = modalQt;
 
-        c('.pizzaInfo--actualPrice').innerHTML = `R$ `+pizzaJson[key].price.toFixed(2);
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ `+pizzaJson[key].price[2].toFixed(2);
 
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';
@@ -70,17 +70,16 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
         c('.pizzaInfo--size.selected').classList.remove('selected');
         size.classList.add('selected');
         if(sizeIndex == 0){
-            c('.pizzaInfo--actualPrice').innerHTML = `R$ `+(pizzaJson[modalKey].price*0.5).toFixed(2);
+            c('.pizzaInfo--actualPrice').innerHTML = `R$ `+pizzaJson[modalKey].price[sizeIndex].toFixed(2);
         } else if(sizeIndex == 1){
-            c('.pizzaInfo--actualPrice').innerHTML = `R$ `+(pizzaJson[modalKey].price*0.8).toFixed(2);
+            c('.pizzaInfo--actualPrice').innerHTML = `R$ `+pizzaJson[modalKey].price[sizeIndex].toFixed(2);
         } else {
-            c('.pizzaInfo--actualPrice').innerHTML = `R$ `+pizzaJson[modalKey].price.toFixed(2);
+            c('.pizzaInfo--actualPrice').innerHTML = `R$ `+pizzaJson[modalKey].price[sizeIndex].toFixed(2);
         }
     });   
 });
 c('.pizzaInfo--addButton').addEventListener('click', ()=>{
     let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
-
     let identifier = pizzaJson[modalKey].id+'@'+size;
     let key = cart.findIndex((item)=>{
         return item.identifier == identifier;
@@ -95,5 +94,78 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
             qt:modalQt
         });
     }
+    updateCart();
     closeModal();
 });
+
+c('.menu-openner').addEventListener('click', ()=>{
+    if(cart.length > 0) {
+        c('aside').style.left = '0'
+    };
+});
+c('.menu-closer').addEventListener('click', ()=>{
+    c('aside').style.left = '100vw';
+});
+
+function updateCart() {
+    c('.menu-openner span').innerHTML = cart.length;
+
+    if(cart.length > 0){
+        c('aside').classList.add('show');
+        c('.cart').innerHTML = '';
+
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
+
+        for(let i in cart) {
+            let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+            subtotal += pizzaItem.price[cart[i].size] * cart[i].qt;
+            console.log(cart);
+
+            let cartItem = c('.models .cart--item').cloneNode(true);
+            let pizzaSizeName;
+            switch(cart[i].size) {
+                case 0:
+                    pizzaSizeName = 'P';
+                    break;
+                case 1:
+                    pizzaSizeName = 'M';
+                    break;
+                case 2:
+                    pizzaSizeName = 'G';
+                    break;
+            };
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+
+            cartItem.querySelector('img').src = pizzaItem.img;
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', ()=>{
+                if(cart[i].qt > 1) {
+                    cart[i].qt--;
+                } else {
+                    cart.splice(i, 1);
+                }
+                updateCart();
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', ()=>{
+                cart[i].qt++;
+                updateCart();
+            });
+
+            c('.cart').append(cartItem);
+        }
+
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto;
+
+        c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
+
+    } else {
+        c('aside').classList.remove('show');
+        c('aside').style.left = '100vw';
+    }
+};
